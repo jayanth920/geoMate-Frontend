b57157638ee4b9b622adedc2ec05ec3175023bd5
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-10-28 15:04:10 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [locations, setLocations] = useState([]);
+
+  const fetchLocations = async () => {
+    try {
+      const response = await axios.get(process.env.REACT_APP_BACKEND_URI);
+      console.log(response.data);
+      setLocations(response.data);
+    } catch (error) {
+      console.error('Error fetching locations:', error);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Fetched Locations</h1>
+      <button onClick={fetchLocations} style={{ marginBottom: '10px' }}>
+        Refresh Locations
+      </button>
+
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        {locations.length > 0 ? (
+          locations.map((loc, index) => (
+            <div key={index} style={{ background: "black", color: "white", marginTop: "20px", marginBottom: "20px" }}>
+              <li>CCode <b>{loc.country_code}</b></li>
+              <li>Country <b>{loc.country}</b></li>
+              <li>State <b>{loc.state}</b></li>
+              <li>District <b>{loc.state_district}</b></li>
+              <li>Village <b>{loc.village}</b></li>
+              <li>Latitude <b>{loc.latitude}</b></li>
+              <li>Longitude <b>{loc.longitude}</b></li>
+            </div>
+          ))
+        ) : (
+          <li>No locations available.</li>
+        )}
+      </ul>
     </div>
   );
 }
